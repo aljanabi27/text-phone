66dfe418d219de96504af8927c341b74611aaf50
abdulmunem <kk6363761@gmail.com>
2024-01-24 14:14:04 +0300

    version 0.3

--- src/main/java/org/example/Contact.java
+++ src/main/java/org/example/Contact.java
@@ -4,21 +4,29 @@ import java.util.ArrayList;
 
 public class Contact {
     private ArrayList<PhoneNumber> listPhoneNumber;
-
+    private int size;
     public Contact(ArrayList<PhoneNumber> listPhoneNumber){
         this.listPhoneNumber = listPhoneNumber;
+        this.size = listPhoneNumber.size();
+    }
+
+    public int getSize() {
+        return size;
     }
 
     public void add(PhoneNumber newPhoneNumber){
         this.listPhoneNumber.add(newPhoneNumber);
+        this.size++;
     }
 
     public void remove(int index){
         listPhoneNumber.remove(index-1);
+        this.size--;
     }
 
     public void removeAll(){
         this.listPhoneNumber = new ArrayList<PhoneNumber>();
+        this.size = 0;
     }
 
     public ArrayList<PhoneNumber> getInformation(){
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,19 +6,43 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner input = new Scanner(System.in);
-    static void add(){
-
+    static void add(Contact contact){
+        String firstName ,lastName,number;
+        System.out.println("Enter first name");
+        firstName = input.next();
+        System.out.println("Enter last name");
+        lastName = input.next();
+        System.out.println("Enter number phone");
+        number = input.next();
+        contact.add(new PhoneNumber(firstName,lastName,number));
+        System.out.println("add this number seccuss");
     }
-    static void remove(){
-
+    static void remove(Contact contact){
+        System.out.println("pleas enter id for number phone that want delete it "+"{"+contact.getSize()+"}");
+        int index = input.nextInt();
+        if(index <= contact.getSize()){
+            contact.remove(index-1);
+            System.out.println("this number is remove");
+        } else {
+            System.out.println("this number out of range");
+        }
     }
-    static void clear(){
-
+    static void clear(Contact contact){
+        System.out.println("is you remove all phone number chosses { true or false }");
+        boolean check = input.nextBoolean();
+        if(check){
+            contact.removeAll();
+            System.out.println("remove all phone number");
+        } else {
+            System.out.println("cancel remove all");
+        }
     }
-    static void printPhoneNumber(){
-
+    static void printPhoneNumber(Contact contact){
+        System.out.println("------------------------------------------------------------------------------------");
+        for(PhoneNumber x:contact.getInformation()){System.out.println(x);}
+        System.out.println("------------------------------------------------------------------------------------");
     }
-    static void Exit(){
+    static void Exit(boolean status){
 
     }
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -32,7 +56,22 @@ public class Main {
 //      create instane of contact object store text Phones and mainpulate them
         Contact contact = new Contact(phoneReader.getPhoneNumber(0));
 //      ------------------------------------------- code ------------------------------------------------
-
+        int x;
+        boolean status = true;
+        while(status){
+            x = input.nextInt();
+            switch (x){
+                case 1:add(contact);
+                break;
+                case 2:remove(contact);
+                break;
+                case 3:printPhoneNumber(contact);
+                break;
+                case 4:clear(contact);
+                break;
+                case 5:status = false;
+            }
+        }
 //      ------------------------------------------- code ------------------------------------------------
 //      write information in the storge file
         PhoneWriter phoneWriter = new PhoneWriter(contact.getInformation(),file);
